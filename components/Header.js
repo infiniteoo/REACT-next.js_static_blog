b3766f2f831e989ee0ef02d5aa5846d2c75f19c5
Troy Dorman <troydorman@gmail.com>
2021-06-29 06:15:29 -0600

    implemented our Header component and integrated it into app.js

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+import Link from 'next/Link'
+
+export default function Header() {
+
+    return (
+        <header>
+          <div className="container">
+          <Link href='/'>
+            <h2>Dev Blog</h2>
+          </Link>
+          </div>
+        </header>
+    )
+}
+
+
